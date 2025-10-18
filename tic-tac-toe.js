4abf38d96bb5d48334c85a8af40f38df9f100218
Darren-JT <darren10212005@gmail.com>
2025-10-17 22:45:16 -0400

    x and o

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,5 @@
+//Part 1
+
 document.addEventListener("DOMContentLoaded", function() {
 
 const board = document.getElementById("board");
@@ -16,7 +18,34 @@ const board = document.getElementById("board");
 
 });
 
+//Part 2
 let currentPlayer = "X";
 
 let gameActive = true;
 let board = Array(9).fill(null);
+
+let i=0;
+while(i < 9) {
+
+
+    squares[i].addEventListener("click", function(){
+
+        if (!gameActive || board[i] !== null) {
+
+
+        squares[i].textContent = currentPlayer;
+        squares[i].classList.add(currentPlayer);
+
+        board[i] = currentPlayer;
+
+        if(currentPlayer === "X") {
+            currentPlayer = "O";
+
+        } 
+        else {
+            currentPlayer = "X";
+        }
+        i++;
+    };
+
+}
